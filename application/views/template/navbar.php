818fe38d7aff526697502f654734f932aad60971
Ryan Duta Pamungkas <arsuma24@gmail.com>
2019-05-08 00:19:58 +0700

    Change Content on Navbar and Comment all custom css

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,7 +1,7 @@
 
 <nav class="uk-navbar-container uk-visible@m uk-padding uk-padding-remove-vertical" uk-navbar>
     <div class="uk-navbar-left">
-        <a class="uk-navbar-item uk-logo" href="#">T E C H P H O R I A</a>
+        <a class="uk-navbar-item uk-logo" href="#">O L M</a>
     </div>
     
     <div class="uk-navbar-right">
